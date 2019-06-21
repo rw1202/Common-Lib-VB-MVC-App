a2bcc167049e38c57630b10cc54952ec68ef3dcf
rw1202 <44084938+rw1202@users.noreply.github.com>
2019-06-21 15:28:47 +0100

    Add files via upload
    Complete

---
+++ Common.Library/DataManagerBase.vb
@@ -0,0 +1,19 @@
+﻿Public Class DataManagerBase
+    Public Function LoadXElement(ByVal partialFileName As String) As XElement
+        Return LoadXElement(partialFileName)
+    End Function
+    Public Function LoadXElement(ByVal partialFileName As String,
+                                 ByVal startingFileName As String) As XElement
+        Dim path As String = IIf (String.IsNullOrEmpty(startingFileName), 
+                                  Environment.CurrentDirectory,startingFileName).ToString()
+        Dim doc As Xelement
+
+        path &= partialFileName
+        path = path.Replace("bin\Debug\", "")
+
+        doc = XElement.Load(path)
+        Return doc
+    End Function
+
+
+End Class
